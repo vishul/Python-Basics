ea1d1254096c7f2c26879eb55558a0415d69a9b2
Vishu <vishvendrasinghrathore@gmail.com>
2016-08-16 17:15:58 +0530

    Delete commandline.py

--- commandline.py
+++
@@ -1,9 +0,0 @@
-#this program illustrates how to accept command line arguments in python
-
-#the package to be included inorder to accpt command line arguments
-from sys import argv
-script, first, second, third = argv
-print script
-print first
-print second
-print third
